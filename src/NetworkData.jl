81121501f187a00253e767c882032ff0665519bf
Eunjong Kim <ekim7206@gmail.com>
2017-09-03 21:55:41 -0700

    redefined permutation ans swapping of port indices

--- src/NetworkData.jl
+++ src/NetworkData.jl
@@ -110,19 +110,36 @@ getindex(D::NetworkData{T}, I1::Tuple{Int, Int},
 getindex(D::NetworkData{T}, I1::Tuple{Int, Int}) where {T<:NetworkParams} =
     getindex(D, I1, :)
 # setindex!: TODO
-
-function swapPorts!(D::NetworkData{T}, i1::Int, i2::Int) where {T<:NetworkParams}
-    D.ports[i1], D.ports[i2] = D.ports[i2], D.ports[i1]
+#
+"""
+Permute port indices
+"""
+function permutePorts!(D::NetworkData{T}, I_before::Vector{Int},
+    I_after::Vector{Int}) where {T<:NetworkParams}
+    if length(unique(I_before)) != length(I_before)
+        error("Error: The indices contained in `I_before` must be unique")
+    end
+    if length(unique(I_after)) != length(I_after)
+        error("Error: The indices contained in `I_after` must be unique")
+    end
+    if sort(I_before) != sort(I_after)
+        error("Error: The arrays `I_before` and `I_after` must contain the same set of indices")
+    end
+    D.ports[I_after] = D.ports[I_before]
     for n in 1:D.nPoint
-        # swap rows
-        D.params[n].data[i1, :], D.params[n].data[i2, :] =
-            D.params[n].data[i2, :], D.params[n].data[i1, :]
-        # swap columns
-        D.params[n].data[:, i1], D.params[n].data[:, i2] =
-            D.params[n].data[:, i2], D.params[n].data[:, i1]
+        # permute rows
+        D.params[n].data[I_after, :] = D.params[n].data[I_before, :]
+        # permute columns
+        D.params[n].data[:, I_after] = D.params[n].data[:, I_before]
     end
     return D
 end
 
+permutePorts(D::NetworkData{T}, I_before::Vector{Int},
+    I_after::Vector{Int}) where {T<:NetworkParams} =
+    permutePorts!(deepcopy(D), I_before, I_after)
+
+swapPorts!(D::NetworkData{T}, i1::Int, i2::Int) where {T<:NetworkParams} =
+    permutePorts!(D, [i1, i2], [i2, i1])
 swapPorts(D::NetworkData{T}, i1::Int, i2::Int) where {T<:NetworkParams} =
-    swapPorts!(deepcopy(_D), i1, i2)
+    swapPorts!(deepcopy(D), i1, i2)
