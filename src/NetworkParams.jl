f269edc097e4f341477ac36d1b92f2c42572d7d7
Eunjong Kim <ekim7206@gmail.com>
2017-09-05 23:03:58 -0700

    Defined binary operations between NetworkParams, moved to NetworkParams.jl. cascading two-port NetworkData was updated.

--- src/NetworkParams.jl
+++ src/NetworkParams.jl
@@ -90,3 +90,29 @@ function show(io::IO, params::NetworkParams)
         write(io, "\n")
     end
 end
+
+function +(param1::T, param2::T) where {T<:NetworkParams}
+    if param1.nPort == param2.nPort
+        return T(param1.data + param2.data)
+    else
+        error("The number of ports must be identical in order to perform binary operations")
+    end
+end
+
+function -(param1::T, param2::T) where {T<:NetworkParams}
+    if param1.nPort == param2.nPort
+        return T(param1.data - param2.data)
+    else
+        error("The number of ports must be identical in order to perform binary operations")
+    end
+end
+
+function *(param1::T, param2::T) where {T<:NetworkParams}
+    if param1.nPort == param2.nPort
+        return T(param1.data * param2.data)
+    else
+        error("The number of ports must be identical in order to perform binary operations")
+    end
+end
+
+^(param::T, N::Int) where {T<:NetworkParams} = T((param.data)^N)
--- src/connect.jl
+++ src/connect.jl
@@ -105,58 +105,8 @@ function _connect_S(A::NetworkData{Sparams}, k::Int,
     return _innerconnect_S(NetworkData(ports, A.frequency, params), k, nA + l)
 end
 
-
-function *(M1::NetworkData{ABCDparams}, M2::NetworkData{ABCDparams})
-    if (M1.frequency == M2.frequency) & (M1.impedance == M2.impedance)
-        nPoint = M1.nPoint
-        M = zeros(Complex128, (2, 2, nPoint))
-        for n in 1:nPoint
-            M[:, :, n] = M1.data[:, :, n] * M2.data[:, :, n]
-        end
-        return NetworkData(ABCDparams, 2, nPoint, M1.impedance, M1.frequency, M)
-    else
-        return error("Operations between data of different
-            frequencies or characteristic impedances not supported")
-    end
-end
-
-function ^(ABCD::NetworkData{ABCDparams}, N::Int)
-    nPoint = ABCD.nPoint
-    ABCDᴺ_data = zeros(Complex128, (2, 2, nPoint))
-    for n in 1:nPoint
-        ABCDᴺ_data[:, :, n] = ABCD.data[:, :, n] ^ N
-    end
-    return NetworkData(ABCDparams, 2, nPoint, ABCD.impedance, ABCD.frequency, ABCDᴺ_data)
-end
-
 """
 Cascade a 2-port touchstone data `Data::NetworkData{T}` `N::Int` times
 """
-cascade(Data::NetworkData{T}, N::Int) where {T<:TwoPortParams} =
-    convert(T, convert(ABCDparams, Data) ^ N)
-
-"""
-Terminate port 2 of a two-port network `s::NetworkData{Sparams}`
-with a one-port touchstone data `t::NetworkData{Sparams, 1}`
-
-s₁₁′ = s₁₁ + s₂₁t₁₁s₁₂ / (1 - t₁₁s₂₂)
-"""
-function terminate(s::NetworkData{Sparams}, t::NetworkData{Sparams})
-    if (s.nPort != 2) | (t.nPort != 1)
-        error("Supported only for the case of a two-port network terminated by a one-port network")
-    end
-    if (s.frequency != t.frequency) | (s.impedance != t.impedance)
-        error("Operations between data of different
-            frequencies or characteristic impedances not supported")
-    end
-
-    s′_data = zeros(Complex128, (1, 1, s.nPoint))
-    s′_data[1, 1, :] = (s.data[1, 1, :] + s.data[2, 1, :] .* t.data[1, 1, :]
-        .* s.data[1, 2, :] ./ (1 - t.data[1, 1, :] .* s.data[2, 2, :]))
-    return NetworkData(Sparams, 1, s.nPoint, s.impedance, s.frequency, s′_data)
-end
-"""
-Method for
-"""
-terminate{T<:NetworkParams, S<:NetworkParams}(s::NetworkData{T},
-    t::NetworkData{S}) = terminate(convert(Sparams, s), convert(Sparams, t))
+cascade(ntwk::NetworkData{ABCDparams}, N::Int) =
+    NetworkData(ntwk.ports, ntwk.frequency, [param^N for param in ntwk.params])
