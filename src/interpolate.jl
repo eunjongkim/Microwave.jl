987e6532ce9f9fadd1785c9db5b37cead96fe5cc
Eunjong Kim <ekim7206@gmail.com>
2017-10-30 15:38:15 -0700

    Created an interpolation function for networkdata

---
+++ src/interpolate.jl
@@ -0,0 +1,21 @@
+using Interpolations
+
+function interpolate_network(ntwk::NetworkData{T}, freq) where {T<:NetworkParams}
+    nPort = ntwk.nPort
+    d = []
+
+    for i in 1:nPort
+        e = []
+        for j in 1:nPort
+            itp = interpolate(ntwk[(i, j), :], BSpline(Cubic(Line())), OnGrid())
+            frng = ntwk.frequency[1]:(ntwk.frequency[end]-ntwk.frequency[1])/(ntwk.nPoint-1):ntwk.frequency[end]
+            sitp = scale(itp, frng)
+#              sitp = scale(itp, ntwk.frequency)
+            push!(e, [sitp[f] for f in freq])
+        end
+        push!(d, e)
+    end
+    params = [T(hcat([[d[i][j][n] for j in 1:nPort] for i in 1:nPort]...)) for n in 1:length(freq)]
+
+    return NetworkData(ntwk.ports, freq, params)
+end
