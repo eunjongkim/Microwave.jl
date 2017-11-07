b1f47644c65fc0a124104da9fe1cf79cbcd28ac0
Eunjong Kim <ekim7206@gmail.com>
2017-11-07 15:41:12 -0800

    added a method for interpolate_data function to work for CircuitData

--- src/interpolate.jl
+++ src/interpolate.jl
@@ -2,6 +2,10 @@ using Interpolations
 
 export interpolate_data
 
+"""
+    interpolate_data(ntwk::NetworkData{T}, freq) where {T<:NetworkParams}
+Interpolate `NetworkData` at a new freqeuncy range `freq`.
+"""
 function interpolate_data(ntwk::NetworkData{T}, freq) where {T<:NetworkParams}
     nPort = ntwk.nPort
     d = []
@@ -19,5 +23,16 @@ function interpolate_data(ntwk::NetworkData{T}, freq) where {T<:NetworkParams}
     end
     params = [T(hcat([[d[i][j][n] for j in 1:nPort] for i in 1:nPort]...)) for n in 1:length(freq)]
 
-    return NetworkData(ntwk.ports, freq, params)
+    return NetworkData(ntwk.ports, collect(freq), params)
+end
+
+"""
+    interpolate_data(cdata::CircuitData{T}, freq) where {T<:CircuitParams}
+Interpolate `CircuitData` at a new freqeuncy range `freq`.
+"""
+function interpolate_data(cdata::CircuitData{T}, freq) where {T<:CircuitParams}
+    itp = interpolate(cdata[:], BSpline(Cubic(Line())), OnGrid())
+    frng = (cdata.frequency[1]):(cdata.frequency[end]-cdata.frequency[1])/(cdata.nPoint-1):(cdata.frequency[end])
+    sitp = scale(itp, frng)
+    return CircuitData(collect(freq), [T(sitp[f]) for f in freq])
 end
