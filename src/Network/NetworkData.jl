78ea8550863601e0592ca94b892d3a86aca3ef42
Eunjong Kim <ekim7206@gmail.com>
2017-11-20 11:07:45 -0800

    minor updates

--- src/Network/NetworkData.jl
+++ src/Network/NetworkData.jl
@@ -2,36 +2,36 @@ export Port, NetworkData, impedances, swap_ports!, permute_ports!
 
 let
     state = 0
-    global p_counter
+    global port_counter
     """
-        p_counter()
+        port_counter()
     Global counter for unique port indices (has to be unique for each port)
     """
-    p_counter() = state += 1
+    port_counter() = state += 1
 end
 
 """
-    Port(impedance::Float64)
-`indPort`: global index of the port. A port index unique in the system is
+    Port{T<:Number}
+`index`: global index of the port. A port index unique in the system is
 assigned whenever an instance of `Port` is created.
 `impedance`: impedance of the port.
 """
 struct Port{T<:Number}
-    indPort::Int
+    index::Int
     impedance::Impedance{T}
-    Port(impedance) = new(p_counter(), impedance)
+    Port(impedance) = new(port_counter(), impedance)
 end
 
 """
     NetworkData{T<:NetworkParams}
 """
-mutable struct NetworkData{T<:NetworkParams}
+mutable struct NetworkData{S<:Real, T<:NetworkParams} <: NetworkData
     nPort::Int
     nPoint::Int
     is_uniform::Bool
-    ports::Array{Port, 1}
-    frequency::Array{Float64, 1}
-    params::Array{T, 1}
+    ports::Vector{Port{N}} where {N<:Number}
+    frequency::Vector{S}
+    params::Vector{T}
     function NetworkData(ports::Array{Port, 1}, frequency,
         params::Array{T,1}) where {T<:NetworkParams}
         nPoint = length(frequency)
@@ -79,11 +79,11 @@ function show(io::IO, D::NetworkData{T}) where {T<:NetworkParams}
     write(io, "\tNumber of datapoints = $(D.nPoint)\n")
     write(io, "\tPort Informaton:\n")
     for (n, p) in enumerate(D.ports)
-        write(io, "\t\tPort $(n) → (global index = $(p.indPort), Z₀ = $(p.impedance))\n")
+        write(io, "\t\tPort $(n) → (global index = $(p.index), Z₀ = $(p.impedance))\n")
     end
 end
 
-impedances(ports::Array{Port, 1}) = [p.impedance for p in ports]
+impedances(ports::Vector{Port{T}}) where {T<:Number} = [p.impedance for p in ports]
 impedances(D::NetworkData) = impedances(D.ports)
 
 """
@@ -111,35 +111,3 @@ getindex(D::NetworkData{T}, I1::Tuple{Int, Int}) where {T<:NetworkParams} =
     getindex(D, I1, :)
 # setindex!: TODO
 #
-"""
-Permute port indices
-"""
-function permute_ports!(D::NetworkData{T}, I_before::Vector{Int},
-    I_after::Vector{Int}) where {T<:NetworkParams}
-    if length(unique(I_before)) != length(I_before)
-        error("Error: The indices contained in `I_before` must be unique")
-    end
-    if length(unique(I_after)) != length(I_after)
-        error("Error: The indices contained in `I_after` must be unique")
-    end
-    if sort(I_before) != sort(I_after)
-        error("Error: The arrays `I_before` and `I_after` must contain the same set of indices")
-    end
-    D.ports[I_after] = D.ports[I_before]
-    for n in 1:D.nPoint
-        # permute rows
-        D.params[n].data[I_after, :] = D.params[n].data[I_before, :]
-        # permute columns
-        D.params[n].data[:, I_after] = D.params[n].data[:, I_before]
-    end
-    return D
-end
-
-permute_ports(D::NetworkData{T}, I_before::Vector{Int},
-    I_after::Vector{Int}) where {T<:NetworkParams} =
-    permute_ports!(deepcopy(D), I_before, I_after)
-
-swap_ports!(D::NetworkData{T}, i1::Int, i2::Int) where {T<:NetworkParams} =
-    permutePorts!(D, [i1, i2], [i2, i1])
-swap_ports(D::NetworkData{T}, i1::Int, i2::Int) where {T<:NetworkParams} =
-    swap_ports!(deepcopy(D), i1, i2)
--- src/Network/operations.jl
+++ src/Network/operations.jl
@@ -24,3 +24,37 @@ function *(param1::T, param2::T) where {T<:NetworkParams}
 end
 
 ^(param::T, N::Int) where {T<:NetworkParams} = T(^(param.data,N))
+
+
+"""
+Permute port indices
+"""
+function permute_ports!(D::NetworkData{T}, I_before::Vector{Int},
+    I_after::Vector{Int}) where {T<:NetworkParams}
+    if length(unique(I_before)) != length(I_before)
+        error("Error: The indices contained in `I_before` must be unique")
+    end
+    if length(unique(I_after)) != length(I_after)
+        error("Error: The indices contained in `I_after` must be unique")
+    end
+    if sort(I_before) != sort(I_after)
+        error("Error: The arrays `I_before` and `I_after` must contain the same set of indices")
+    end
+    D.ports[I_after] = D.ports[I_before]
+    for n in 1:D.nPoint
+        # permute rows
+        D.params[n].data[I_after, :] = D.params[n].data[I_before, :]
+        # permute columns
+        D.params[n].data[:, I_after] = D.params[n].data[:, I_before]
+    end
+    return D
+end
+
+permute_ports(D::NetworkData{T}, I_before::Vector{Int},
+    I_after::Vector{Int}) where {T<:NetworkParams} =
+    permute_ports!(deepcopy(D), I_before, I_after)
+
+swap_ports!(D::NetworkData{T}, i1::Int, i2::Int) where {T<:NetworkParams} =
+    permutePorts!(D, [i1, i2], [i2, i1])
+swap_ports(D::NetworkData{T}, i1::Int, i2::Int) where {T<:NetworkParams} =
+    swap_ports!(deepcopy(D), i1, i2)
