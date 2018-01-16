e28527b3ba88b7d2f9f0ec67d4186027c2e85712
Eunjong Kim <ekim7206@gmail.com>
2018-01-15 23:18:18 -0800

    bug fix.

--- src/Circuit/CircuitParams.jl
+++ src/Circuit/CircuitParams.jl
@@ -5,8 +5,8 @@ abstract type CircuitParams{T<:Real} <: AbstractParams end
 """
 mutable struct Impedance{T<:Real} <: CircuitParams{T}
     data::Complex{T}
-    Impedance(z::T) where {T<:Real} = Impedance(complex(z))
 end
+Impedance(z::T) where {T<:Real} = Impedance(complex(z))
 
 Impedance(zd::AbstractVector{Complex{T}}) where {T<:Real} =
     [Impedance(zd_) for zd_ in zd]
@@ -17,8 +17,8 @@ Impedance(zd::AbstractVector{T}) where {T<:Real} = Impedance(Complex{T}.(zd))
 """
 mutable struct Admittance{T<:Real} <: CircuitParams{Real}
     data::Complex{T}
-    Admittance(y::T) where {T<:Real} = Admittance(complex(y))
 end
+Admittance(y::T) where {T<:Real} = Admittance(complex(y))
 
 Admittance(yd::AbstractVector{Complex{T}}) where {T<:Real} =
     [Admittance(yd_) for yd_ in yd]
